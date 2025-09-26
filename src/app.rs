8763cfbb45e6858db8b57be041702ad4705e5acb
Mohhamad <mohhamad.fa@gmail.com>
2025-09-26 13:26:53 +0330

    add required code to produce the problem
    extra info in readme

--- src/app.rs
+++ src/app.rs
@@ -1,6 +1,10 @@
+use leptos::server;
 use leptos::{
     IntoView, component,
     html::{ElementChild, p},
+    logging::log,
+    prelude::{RwSignal, ServerFnError, Update, provide_context},
+    server::OnceResource,
 };
 use leptos_meta::provide_meta_context;
 
@@ -9,5 +13,34 @@ use crate::problematic_router::RootsOfProblems;
 #[component]
 pub fn App() -> impl IntoView {
     provide_meta_context();
+    provide_current_user_context();
     (p().child("app page"), RootsOfProblems())
+}
+
+#[derive(Clone, Debug, PartialEq)]
+pub struct CurrentUser {
+    pub me: Option<String>,
+}
+
+fn provide_current_user_context() {
+    let current_user = RwSignal::new(CurrentUser { me: None });
+    OnceResource::new_blocking(async move {
+        let response = get_current_user_private_data().await;
+        match response {
+            Ok(value) => {
+                current_user.update(|input| {
+                    input.me = Some(value);
+                });
+            }
+            Err(error) => {
+                log!("{error:?}");
+            }
+        }
+    });
+    provide_context(current_user);
+}
+
+#[server]
+async fn get_current_user_private_data() -> Result<String, ServerFnError> {
+    Ok(String::from("The User"))
 }
--- src/page_with_problem.rs
+++ src/page_with_problem.rs
@@ -1,60 +1,51 @@
-use leptos::html::br;
-use leptos::server;
+use leptos::ev;
+use leptos::logging::log;
+use leptos::prelude::{RwSignal, ToChildren, Transition, TransitionProps, Update, use_context};
 use leptos::{
-    IntoView, component, ev,
-    html::{ElementChild, button, div, p},
-    prelude::{
-        ClassAttribute, Get, OnAttribute, ServerFnError, Suspense, SuspenseProps, ToChildren, Write,
-    },
-    reactive::signal::signal,
+    IntoView, component,
+    html::{ElementChild, button, p},
+    prelude::{Get, OnAttribute},
     server::Resource,
 };
-use leptos_meta::provide_meta_context;
+
+use crate::app::CurrentUser;
 
 #[component]
 pub fn ProblematicPage() -> impl IntoView {
-    provide_meta_context();
-
-    let (count, count_ser) = signal(0);
-    let resource1 = Resource::new(move || count.get(), |input| async move { input * 2 });
-    let resource2 = Resource::new_blocking(
-        move || count.get(),
-        |input| async move {
-            wait_on_server().await.unwrap();
-            input * 3
+    let user = use_context::<RwSignal<CurrentUser>>();
+    let logged_in = Resource::new_blocking(
+        move || user.get(),
+        |input| async {
+            log!("{:?}", input);
+            let logged_in = match input {
+                Some(user) => match user.me {
+                    Some(_) => true,
+                    None => false,
+                },
+                None => false,
+            };
+            logged_in
         },
     );
-    div().class("bg-white h-full p-5").child((
-        p().child("First one, Good luck"),
-        br(),
-        p().child(move || format!("count: {}", count.get())),
-        Suspense(
-            SuspenseProps::builder()
-                .fallback(|| p().child("loading suspense 1 ..."))
+    (
+        Transition(
+            TransitionProps::builder()
                 .children(ToChildren::to_children(move || {
-                    p().child(move || format!("suspense data 1: {:?}", resource1.get()))
+                    p().child(("logged in state: ", move || logged_in.get()))
                 }))
                 .build(),
         ),
-        Suspense(
-            SuspenseProps::builder()
-                .fallback(|| p().child("loading suspense 2 ..."))
-                .children(ToChildren::to_children(move || {
-                    p().child(move || format!("suspense data 2: {:?}", resource2.get()))
-                }))
-                .build(),
-        ),
-        button().child("button").on(ev::click, move |_| {
-            *count_ser.write() += 1;
+        button().child("update context").on(ev::click, move |_| {
+            log!("update context click");
+            let current_user = use_context::<RwSignal<CurrentUser>>().unwrap();
+            current_user.update(|input| {
+                input.me = None;
+            });
+            let current_user = use_context::<RwSignal<CurrentUser>>();
+            log!(
+                "inside callback, context value after update: {:?}",
+                current_user.get()
+            )
         }),
-    ))
-}
-
-#[server]
-async fn wait_on_server() -> Result<(), ServerFnError> {
-    use std::time::Duration;
-    use tokio::time::sleep;
-
-    sleep(Duration::from_secs(2)).await;
-    Ok(())
+    )
 }
